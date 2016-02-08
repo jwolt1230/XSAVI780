f8e38d447c01a8ed201d58c2b9c2559730228fe5
jwolt1230 <jwolterding@gmail.com>
2016-02-08 18:18:47 -0500

    Assignment 1
    Html, JS, CSS

---
+++ script.js
@@ -0,0 +1,5 @@
+var thingsILikeToDo = ["eating", "drinking", "reading", "sleeping"];
+		console.log(thingsILikeToDo[0]);
+		console.log(thingsILikeToDo[1]);
+		console.log(thingsILikeToDo[2]);
+		console.log(thingsILikeToDo[3]);
